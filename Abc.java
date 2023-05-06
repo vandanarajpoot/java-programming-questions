b5012c19d0d842230356a56c60c8102c93ecaf42
monulodhi <monulodhi849@gmail.com>
2023-05-06 14:53:04 +0530

    file deleted

--- Abc.java
+++
@@ -1,5 +0,0 @@
-public class Abc {
-    public static void main(String args[]) {
-        System.out.println("hello this is varun here");
-    }
-}
--- Subtraction.java
+++
@@ -1,6 +0,0 @@
-public class Subtraction {
-    public static void main(String args[]) {
-        int x = 10-5;
-        System.out.println(x);
-    }
-}
