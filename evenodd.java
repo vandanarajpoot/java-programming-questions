7913211ba4996a1a43f12e274718a18152dd2b35
monulodhi <monulodhi849@gmail.com>
2023-05-10 14:44:08 +0530

    file add

---
+++ evenodd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in) ;
+    System.out.println(" enter no.");
+    int a = sc.nextInt();
+    if (a%2==0) {
+       System.out.println("no. is even");
+      } else {
+       System.out.println("no. is odd");
+      }
+    }
+  }
